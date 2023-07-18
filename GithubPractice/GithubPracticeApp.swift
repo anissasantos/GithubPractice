91b1486e254e651bfe82ec9283cb63afc9713a90
Anissa Santos <anissa.n.santos@gmail.com>
2023-07-18 09:28:10 -0500

    Initial Commit

---
+++ GithubPractice/GithubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPracticeApp.swift
+//  GithubPractice
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
